26ea01100f0cd9c7053ba09739216ff7e1ea6881
Stanley Chen <stanley.chen@trustscience.com>
2017-10-27 12:04:14 -0700

    swagger working at http://localhost:<port>/swagger/index.html

--- SwaggerApiCore/Startup.cs
+++ SwaggerApiCore/Startup.cs
@@ -8,6 +8,9 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
+using Microsoft.Extensions.PlatformAbstractions;
+using System.IO;
 
 namespace SwaggerApiCore
 {
@@ -24,6 +27,16 @@ namespace SwaggerApiCore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+
+            var pathToDoc = Configuration["Swagger:FileName"];
+
+            services.AddSwaggerGen(s =>
+            {
+                s.SwaggerDoc("v1", new Info { Title = "ApiServices", Version = "v1" });
+                s.DescribeAllEnumsAsStrings();
+            });
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,9 +45,27 @@ namespace SwaggerApiCore
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
             }
 
-            app.UseMvc();
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+
+            app.UseSwagger(c =>
+            {
+                c.PreSerializeFilters.Add((swagger, httpReq) => swagger.Host = httpReq.Host.Value);
+            });
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "V1 Docs");
+            });
         }
     }
 }
